c83df29c1473b76c47be310f67d735510b77b310
yogan-gopi <yogang2002@gmail.com>
2023-03-14 14:53:24 +0530

    Home changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,19 @@ import './Home.css';
 import '../index.css';
 import axios from 'axios';
 
-const sendString = async(inputString) => {
-    const response = await axios.post('https//localhost:5000', {
-        text: inputString
-    })
-    return response.data
-}
 
 const Home = () => {
+
+    const [data, setData] = useState()
+    const [text, setText] = useState()
+    
+    const sendString = async() => {
+        const response = await axios.post('http://localhost:5000', {
+            text: text
+        })
+    console.log(response)
+    //setData(response)
+    }
 
     window.addEventListener('scroll', ()=>{
         const f = document.getElementById('logo')
@@ -22,9 +27,9 @@ const Home = () => {
         <div className="flex flex-col justify-center container-div mx-auto items-center">
             <h1 className="logo" id="logo">Dr.Det</h1>
             <p className="desc">A Tool for Fake News Detection</p>
-            <textarea className="border border-solid border-slate-500 textarea" id="data" name="data" ></textarea>
+            <textarea className="border border-solid border-slate-500 textarea" id="data" name="data" onChange={e => setText(e.target.value)}></textarea>
             <div>
-                <button className="rounded-md button my-10 mx-5" id="button">Submit</button>
+                <button className="rounded-md button my-10 mx-5" id="button" onClick={sendString}>Submit</button>
                 <button className="rounded-md button-clear my-10 mx-5" id="button-clear">Clear</button>
             </div>
             <div className="resultstyle px-10 rounded-full  ">
